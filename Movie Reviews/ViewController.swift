a0a28aa65e617509f6625638e7f84c93893fc689
Mariella Sypa <mariella.725@icloud.com>
2016-01-18 13:15:20 -0500

    Initial Commit

---
+++ Movie Reviews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie Reviews
+//
+//  Created by Mariella Sypa on 1/18/16.
+//  Copyright © 2016 Mariella Sypa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
